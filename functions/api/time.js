ebd64abcaf534e9569a973e6f1b3deae192c39ac
gitee <xuhx20.qq.com>
2022-10-04 18:08:59 +0800

    init

--- functions/api/time.js
+++ functions/api/time.js
@@ -9,7 +9,7 @@ export async function onRequest(context) {
       data, // arbitrary space for passing data between middlewares
     } = context;
   
-  
+    console.log(context)
     return new Response("Hello, world!");
   
   }
